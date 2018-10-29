2e54489e378c8b0659c9253799d5f874f4a4393d
cmunerap <cmunerap@gmail.com>
2018-10-28 20:59:55 -0500

    Added configuration to run on multiple browsers

--- protractor/saucelabs.config.ts
+++ protractor/saucelabs.config.ts
@@ -1,7 +1,25 @@
 import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
+const firefoxConfig = {
+  browserName: 'firefox',
+  // platform: 'linux',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
 export const config: Config = {
+  multiCapabilities,
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   getPageTimeout: 30000,
@@ -11,14 +29,6 @@ export const config: Config = {
   SELENIUM_PROMISE_MANAGER: false,
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-  capabilities: {
-    name: 'UI Workshop',
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ['disable-infobars=true', '--window-size=800,600'],
-      prefs: { credentials_enable_service: false }
-    }
-  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     browser.manage().timeouts().implicitlyWait(0);
